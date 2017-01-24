9299b2e94d0e9ff37d41f64e3ca43337051d5180
Raphael Moraes <raphael.lsmoraes@gmail.com>
2017-01-24 02:03:03 -0200

    Initial commit for vnt4tech base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LampComponent } from './lamp/lamp.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LampComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule,
